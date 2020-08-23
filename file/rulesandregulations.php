6671ffeea3f9435f16110b337838ee3aba2006b9
AromalPS <19gce27@meaec.edu.in>
2020-08-23 23:08:44 +0530

    rulesandregulation_page_added

---
+++ file/rulesandregulations.php
@@ -0,0 +1,58 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="css/bootstrap.min.css" type="text/css" />
+    <link rel="stylesheet" href="css/style.css" type="text/css">
+    <title>Document</title>
+</head>
+<body>
+<!-- <nav class="navbar navbar-inverse navbar-fixed-top" role="navigation">
+	<div class="container-fluid">
+		add header
+		<div class="navbar-header">
+			<button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#navbar1">
+				<span class="sr-only">Toggle navigation</span>
+				<span class="icon-bar"></span>
+				<span class="icon-bar"></span>
+				<span class="icon-bar"></span>
+			</button>
+			<a class="navbar-brand" href=""><h3><b>&nbsp;Lachas Trezò </b></h3>
+</a>
+		</div>
+		menu items
+		<div class="collapse navbar-collapse" id="navbar1">
+			<ul class="nav navbar-nav navbar-right">
+				
+				<li class="active"><a href="">New User</a></li>
+				<li><a href="login.php">Login</a></li>
+			</ul>
+		</div>
+	</div>
+</nav> -->
+
+
+<div id="banner_image">
+    <div class="col-lg-3"><a href="/rulesandregulations.php"><h1 style="color:antiquewhite">Lachas Trezò</h1></a></div>
+
+            <div class="container">
+                <center>
+                <div id="banner_content">
+                <h2 style="color:#f2aa4cff"><u><b>Rules & Regulations</b></u><br></h2><p><h4 >
+      1. You can participate through Mobile phone or PC.<br><br>
+        2. You will have the respective username and password for each one.<br><br>
+        3. There will be 9 exciting questions.<br><br>
+        4. You have only one day to participate & complete all questions.<br><br> 
+        5. Those who completely answered all the questions with minimum time will be announced as the winner.<br><br>
+       
+
+      </h4>
+                    <a href="login.php" class="btn btn-primary btn-lg active">Login</a>&nbsp &nbsp &nbsp&nbsp &nbsp &nbsp
+                    <a href="index.php" class="btn btn-danger btn-lg active">Sign up</a>
+                </div>
+                </center>
+            </div>
+</div>
+</body>
+</html>
