2c47174202ac4e67e70e03d0f735fd88e549f9a0
AromalPS <19gce27@meaec.edu.in>
2020-08-24 00:35:49 +0530

    Update

--- file/index.php
+++ file/index.php
@@ -19,10 +19,11 @@ document.signupform.b1.value=l;
 	<link rel="icon" href="http://meaieeesb.org/xtrinia/wp-content/uploads/2016/01/cropped-xtrinia-logo-mea-ieee-sb-1-2-180x180.png" type="image/x-icon" width='100%'>
 	<meta content="width=device-width, initial-scale=1.0" name="viewport" >
 	<link rel="stylesheet" href="css/bootstrap.min.css" type="text/css" />
+	<link rel="stylesheet" href="css/style.css" type="text/css">
 </head>
 <body>
 
-<nav class="navbar navbar-default" role="navigation">
+<nav class="navbar navbar-inverse navbar-fixed-top" role="navigation">
 	<div class="container-fluid">
 		<!-- add header -->
 		<div class="navbar-header">
@@ -45,14 +46,16 @@ document.signupform.b1.value=l;
 		</div>
 	</div>
 </nav>
-
-<div class="container">
+<div id="banner_image2">
+    <div class="container">
+	<center>
+	<div id="banner_content2">
 	<div class="row">
-		<div class="col-md-4 col-md-offset-4 well">
+		<div class="col-md-6 col-md-offset-3 well">
 			<form role="form" action="startu.php" method="post" name="signupform">
 				<fieldset>
 					<legend>Let's start</legend>
-<form name="signupform">
+        			<form name="signupform">
 					<div class="form-group">
 						<label for="name">Name</label>
 						<input type="text" name="b1" placeholder="*Name" required  class="form-control" />
@@ -83,7 +86,7 @@ document.signupform.b1.value=l;
 				
 
 					<div class="form-group">
-						<input onclick="checking();" type="submit" name="signup" value="Register" class="btn btn-primary" />
+						<input onclick="checking();" type="submit" name="signup" value="Register" class="btn btn-primary">
 					</div>
 				</fieldset>
 			</form>
@@ -91,11 +94,13 @@ document.signupform.b1.value=l;
 			<span class="text-danger"></span>
 		</div>
 	</div>
-	
+	<div class="row">
+	<div class="col-md-4 col-md-offset-4 text-center">
+	Already registered then <a href="login.php">Click Here</a>
+	</div>
+	</div>
+	</center>
 </div>
-<div class="row">
-<div class="col-md-4 col-md-offset-4 text-center">
-Already registered then <a href="login.php">Click Here</a>
 </div>
 </div>
 <script src="js/jquery-1.10.2.js"></script>
